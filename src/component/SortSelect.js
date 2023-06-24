ef2ebe183d87d8fceb599f9d2aede289aebc5c42
Deepanshi Sharma <deepanshisharma2510@gmail.com>
2023-06-24 22:55:04 +0530

    feat: sort posts

--- src/component/SortSelect.js
+++ src/component/SortSelect.js
@@ -1,13 +1,30 @@
+import { forumData } from "../db/posts";
+import { usePostContext } from "../context/PostContext";
 export default function SortSelect() {
-  const handleInput = () => {};
+  const { setData } = usePostContext();
+  const handleInput = (e) => {
+    let sortedPosts = forumData.posts.slice();
+    if (e.target.value === "time") {
+      sortedPosts = [...sortedPosts].sort(
+        (a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt)
+      );
+      return setData((prevData) => ({ ...prevData, posts: sortedPosts }));
+    }
+    if (e.target.value === "votes") {
+      sortedPosts = [...sortedPosts].sort(
+        (a, b) => b.upvotes - b.downvotes - (a.upvotes - b.downvotes)
+      );
+      return setData((prevData) => ({ ...prevData, posts: sortedPosts }));
+    }
+  };
   return (
     <div>
       <select class="custom-select" onChange={(e) => handleInput(e)}>
         <option selected disabled>
           choose
         </option>
-        <option value="option1">Latest Posts</option>
-        <option value="option2">Most Upvoted Posts</option>
+        <option value="time">Latest Posts</option>
+        <option value="votes">Most Upvoted Posts</option>
       </select>
     </div>
   );
